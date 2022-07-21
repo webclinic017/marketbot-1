32e684d4a6a239f699fb68ad405220b48b9e0987
Jack Steussie <jsteussi@ucsd.edu>
2022-07-21 00:54:22 -0700

    Project creation

---
+++ api/creds.py
@@ -0,0 +1,42 @@
+import logging
+from tda import auth
+from tda.auth import Client as tda_Client
+from coinbase.wallet.client import Client as cb_Client
+from configparser import ConfigParser
+from typing import Union
+
+def client_connect(api, log=False, config='./config/config.ini') -> Union[cb_Client, tda_Client]:
+    '''
+        Returns a client connection to Coinbase API or TD Ameritrade API and 
+        authenticates the connection with the given keys accordingly. 
+
+        Args:
+            api (string) : the api to connect to (either 'TDA' or 'COINBASE')
+            log (bool) : enable/disable logging for debugging authentication
+            config (string) : path to the config file with auth keys 
+        
+        Returns:
+            A Client object from either the TDA API or the Coinbase API
+
+        Raises:
+            AnyError : if connection was unable to be established/authenticated
+    '''
+    logging.getLogger('').addHandler(logging.StreamHandler())
+    config = ConfigParser()
+    config.read('private/config.ini')
+    API_KEY = config.get(f'{api}_AUTH', 'API_KEY')
+    if api == 'TDA':
+        REDIRECT = config.get(f'{api}_AUTH', 'REDIRECT')
+        TOKEN_PATH = config.get(f'{api}_AUTH', 'TOKEN_PATH')
+        try:
+            client = auth.client_from_token_file(TOKEN_PATH, API_KEY)
+        except FileNotFoundError:
+            from selenium import webdriver
+            with webdriver.Chrome() as driver:
+                client = auth.client_from_login_flow(
+                    driver, API_KEY, REDIRECT, TOKEN_PATH)
+        return client
+    elif api == 'COINBASE':
+        API_SECRET = config.get('COINBASE_AUTH', 'API_SECRET')
+        client = cb_Client(API_KEY, API_SECRET)
+        return client
